75de07973c3615dcfe92438247aa1fc65451b826
Saud <=>
2023-11-14 02:31:12 +0530

    Add site logo to footer and adjust layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,24 +10,29 @@ const Footer: React.FC = () => {
       theme: "dark",
       hideProgressBar: true,
       style: {
-
         color: "#99908D",
         fontSize: "12px",
         minWidth: "250px"
       }
     });
-      };
+  };
   return (
-    <div className='h-40 w-full flex flex-col justify-center'>
-      <p className='text-lightTextColor/50'>
-        Built with <Link href="https://react.dev/" label='React' />, {" "}
-        <Link href="https://www.typescriptlang.org/" label='TypeScript' />, {" "}
-        <Link href="https://tailwindcss.com/" label='Tailwind' />, and {" "}
-        <Link href="https://vercel.com/" label='Vercel' />.
-      </p>
-      <p className='text-lightTextColor/50'>
-        Contact me on {email} <VscCopy className='inline cursor-pointer' onClick={copy_email} />
-      </p>
+    <div className='flex gap-2 items-center'>
+      <div>
+        <img className='w-8 h-8' src='https://res.cloudinary.com/dafq69nvu/image/upload/v1699906820/logo_hcrw1f.svg' alt='Site Logo' />
+      </div>
+
+      <div className='h-40 w-full flex flex-col justify-center'>
+        <p className='text-lightTextColor/50'>
+          Built with <Link href="https://react.dev/" label='React' />, {" "}
+          <Link href="https://www.typescriptlang.org/" label='TypeScript' />, {" "}
+          <Link href="https://tailwindcss.com/" label='Tailwind' />, and {" "}
+          <Link href="https://vercel.com/" label='Vercel' />.
+        </p>
+        <p className='text-lightTextColor/50'>
+          Contact me on {email} <VscCopy className='inline cursor-pointer' onClick={copy_email} />
+        </p>
+      </div>
     </div>
   )
 };
